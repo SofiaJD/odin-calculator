3b7d8ad79789cf6d1dc65de603462199654f4943
Sofía J. D. <59852487+SofiaJD@users.noreply.github.com>
2024-01-28 19:49:48 -0400

    Add operate funtion

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1 = 0,
+    operator, 
+    num2 = 0;
+
 function add(num1, num2)
 {
     let total = num1 + num2;
@@ -20,4 +24,26 @@ function divide(num1, num2)
 {
     let total = num1 / num2;
     return total;
+}
+
+function operate(operator, num1, num2)
+{
+    switch(operator)
+    {
+        case '+':
+            add(num1, num2);
+        break;
+
+        case '-':
+            subtract(num1, num2);
+        break;
+
+        case '*':
+            multiply(num1, num2);
+        break;
+
+        case '/':
+            divide(num1, num2);
+        break;
+    }
 }
