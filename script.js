ed436eaa8dc015032a8a0983e36cc7fe600bc3d2
Sofía J. D. <59852487+SofiaJD@users.noreply.github.com>
2024-02-02 00:52:37 -0400

    Add functionality to the calculator

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const screen = document.querySelector('#screen');
+const btnNumbers = document.querySelectorAll('.number');
+const btnOperations = document.querySelectorAll('.operation');
+const btnEqual = document.querySelector('#btnEqual');
+const btnClear = document.querySelector('#btnClear');
+
 let num1 = 0,
     operator, 
     num2 = 0;
@@ -31,19 +37,79 @@ function operate(operator, num1, num2)
     switch(operator)
     {
         case '+':
-            add(num1, num2);
+            return add(num1, num2);
         break;
 
         case '-':
-            subtract(num1, num2);
+            return subtract(num1, num2);
         break;
 
         case '*':
-            multiply(num1, num2);
+            return multiply(num1, num2);
         break;
 
         case '/':
-            divide(num1, num2);
+            return divide(num1, num2);
         break;
     }
 }
+
+function cleanScreen()
+{
+    screen.textContent = '';
+}
+
+function fillScreen()
+{
+    btnNumbers.forEach(btnNumber => 
+        {
+            btnNumber.addEventListener('click', function()
+            {
+                cleanScreen();
+                screen.textContent += btnNumber.value;
+
+                if(operator === undefined)
+                {
+                    num1 = screen.textContent;
+                }
+                else 
+                {
+                    num2 = screen.textContent;
+                }
+                
+            });
+        });
+}
+
+function calc()
+{
+    fillScreen();
+
+    btnOperations.forEach(btnOperation => 
+        {
+            btnOperation.addEventListener('click', function()
+            {
+                if(operator != undefined)
+                {
+                    screen.textContent = operate(operator, parseInt(num1), parseInt(num2));
+                    num1 = screen.textContent, num2 = 0, operator = btnOperation.value;
+                }
+                else 
+                {
+                    operator = btnOperation.value;
+                    cleanScreen();
+                }
+            });
+        });
+}
+
+btnEqual.addEventListener('click', function()
+{
+    cleanScreen();
+    screen.textContent = operate(operator, parseInt(num1), parseInt(num2));
+    num1 = screen.textContent, num2 = 0, operator = undefined, result = 0;
+});
+
+btnClear.addEventListener('click', cleanScreen);
+
+calc();
